88475a11d223fe0e53485ace0d53415a6c416240
krethick <krethick@gmail.com>
2024-07-13 17:27:05 +0530

    Completed Smooth Enemy Movement

--- Assets/_Enemy/EnemyMover.cs
+++ Assets/_Enemy/EnemyMover.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,7 +6,7 @@ using UnityEngine;
 public class EnemyMover : MonoBehaviour
 {
     [SerializeField] List<Waypoint> path = new List<Waypoint>(); // Takes the waypoint script and creates a List
-    [SerializeField] float waitTime = 1f; // Waiting time for 1 second
+    [SerializeField] [Range(0f,5f)] float speed = 1f; // use range amd Waiting time for speed at 1 second
     void Start()
     {
         StartCoroutine(FollowPath());
@@ -15,8 +16,19 @@ public class EnemyMover : MonoBehaviour
     {
         foreach(Waypoint waypoint in path) // Looping elements within that list
         {
-            transform.position =  waypoint.transform.position; 
-            yield return new WaitForSeconds(waitTime);
+            Vector3 startposition = transform.position;  // Starting Position
+            Vector3 endPosition = waypoint.transform.position; // Ending Position
+            float travelPercent = 0f; // travel percent
+            
+            transform.LookAt(endPosition);
+
+            while(travelPercent < 1f) // We are not the end position
+            {
+            travelPercent += Time.deltaTime * speed;
+            transform.position = Vector3.Lerp(startposition, endPosition,travelPercent);
+            yield return new WaitForEndOfFrame(); // End of the frame is completed
+            }
+            
         }
     }
 }
